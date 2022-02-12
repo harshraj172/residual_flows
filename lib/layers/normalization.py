453c7dba6cca4c34fc5195ea89999a50a4504c3a
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-02-12 23:10:35 +0530

    Update normalization.py

--- lib/layers/normalization.py
+++ lib/layers/normalization.py
@@ -31,7 +31,7 @@ class MovingBatchNormNd(nn.Module):
         if self.affine:
             self.bias.data.zero_()
 
-    def forward(self, x, logpx=None):
+    def forward(self, x, logpx=None, _logdetgrad_=None):
         c = x.size(1)
         used_mean = self.running_mean.clone().detach()
 
@@ -61,9 +61,12 @@ class MovingBatchNormNd(nn.Module):
         if logpx is None:
             return y
         else:
-            return y, logpx
+            if _logdetgrad_ is None:
+                return y, logpx
+            else:
+                return y, logpx, _logdetgrad_
 
-    def inverse(self, y, logpy=None):
+    def inverse(self, y, logpy=None, _logdetgrad_=None):
         used_mean = self.running_mean
 
         if self.affine:
@@ -76,7 +79,10 @@ class MovingBatchNormNd(nn.Module):
         if logpy is None:
             return x
         else:
-            return x, logpy
+            if _logdetgrad_ is None:
+                return x, logpy
+            else:
+                return x, logpy, _logdetgrad_ 
 
     def __repr__(self):
         return (
