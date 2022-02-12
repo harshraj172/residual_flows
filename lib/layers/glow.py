b6a85fc34b0a1d8249aab491d83cff13b0a0532c
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-02-12 13:32:53 +0530

    Update glow.py

--- lib/layers/glow.py
+++ lib/layers/glow.py
@@ -10,22 +10,22 @@ class InvertibleLinear(nn.Module):
         self.dim = dim
         self.weight = nn.Parameter(torch.eye(dim)[torch.randperm(dim)])
 
-    def forward(self, x, logpx=None, logdetgrad=None):
+    def forward(self, x, logpx=None, _logdetgrad_=None):
         y = F.linear(x, self.weight)
         if logpx is None:
             return y
         else:
-            if logdetgrad is None:
+            if _logdetgrad_ is None:
                 return y, logpx - self._logdetgrad
             else:
                 return y, logpx - self._logdetgrad, self._logdetgrad
 
-    def inverse(self, y, logpy=None, logdetgrad=None):
+    def inverse(self, y, logpy=None, _logdetgrad_=None):
         x = F.linear(y, self.weight.inverse())
         if logpy is None:
             return x
         else:
-            if logdetgrad is None:
+            if _logdetgrad_ is None:
                 return x, logpy + self._logdetgrad
             else:
                 return x, logpy + self._logdetgrad, self._logdetgrad
@@ -45,22 +45,22 @@ class InvertibleConv2d(nn.Module):
         self.dim = dim
         self.weight = nn.Parameter(torch.eye(dim)[torch.randperm(dim)])
 
-    def forward(self, x, logpx=None, logdetgrad=None):
+    def forward(self, x, logpx=None, _logdetgrad_=None):
         y = F.conv2d(x, self.weight.view(self.dim, self.dim, 1, 1))
         if logpx is None:
             return y
         else:
-            if logdetgrad is None:
+            if _logdetgrad_ is None:
                 return y, logpx - self._logdetgrad.expand_as(logpx) * x.shape[2] * x.shape[3]
             else:
                 return y, logpx - self._logdetgrad.expand_as(logpx) * x.shape[2] * x.shape[3], self._logdetgrad.expand_as(logpx) * x.shape[2] * x.shape[3]
 
-    def inverse(self, y, logpy=None, logdetgrad=None):
+    def inverse(self, y, logpy=None, _logdetgrad_=None):
         x = F.conv2d(y, self.weight.inverse().view(self.dim, self.dim, 1, 1))
         if logpy is None:
             return x
         else:
-            if logdetgrad is None: 
+            if _logdetgrad_ is None: 
                 return x, logpy + self._logdetgrad.expand_as(logpy) * x.shape[2] * x.shape[3]
             else:
                 return x, logpy + self._logdetgrad.expand_as(logpy) * x.shape[2] * x.shape[3], self._logdetgrad.expand_as(logpy) * x.shape[2] * x.shape[3] 
