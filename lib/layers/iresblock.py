f4b3a29bbcd8b35a194f97d6123d6338ab20dca7
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-02-12 13:34:28 +0530

    Update iresblock.py

--- lib/layers/iresblock.py
+++ lib/layers/iresblock.py
@@ -51,23 +51,23 @@ class iResBlock(nn.Module):
         self.register_buffer('last_firmom', torch.zeros(1))
         self.register_buffer('last_secmom', torch.zeros(1))
 
-    def forward(self, x, logpx=None, _logdetgrad=None):
+    def forward(self, x, logpx=None, _logdetgrad_=None):
         if logpx is None:
             y = x + self.nnet(x)
             return y
         else:
             g, logdetgrad = self._logdetgrad(x)
-            if _logdetgrad is None:
+            if _logdetgrad_ is None:
                 return x + g, logpx - logdetgrad
             else:
                 return x + g, logpx - logdetgrad, logdetgrad
 
-    def inverse(self, y, logpy=None, _logdetgrad=None):
+    def inverse(self, y, logpy=None, _logdetgrad_=None):
         x = self._inverse_fixed_point(y)
         if logpy is None:
             return x
         else:
-            if _logdetgrad is None:
+            if _logdetgrad_ is None:
                 return x, logpy + self._logdetgrad(x)[1]
             else:
                 return x, logpy + self._logdetgrad(x)[1], self._logdetgrad(x)[1]
