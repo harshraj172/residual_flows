2b33b1c9592c6531e23a3f01fdc9b583dfae47f0
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-02-15 21:29:01 +0530

    Update container.py

--- lib/layers/container.py
+++ lib/layers/container.py
@@ -9,35 +9,25 @@ class SequentialFlow(nn.Module):
         super(SequentialFlow, self).__init__()
         self.chain = nn.ModuleList(layersList)
 
-    def forward(self, x, logpx=None, _logdetgrad_=None):
+    def forward(self, x, logpx=None):
         if logpx is None:
             for i in range(len(self.chain)):
                 x = self.chain[i](x)
             return x
         else:
-            if _logdetgrad_ is None:
-                for i in range(len(self.chain)):
-                    x, logpx = self.chain[i](x, logpx)
-                return x, logpx
-            else:
-                for i in range(len(self.chain)):
-                    x, logpx, _logdetgrad_ = self.chain[i](x, logpx, _logdetgrad_)
-                return x, logpx, _logdetgrad_
+            for i in range(len(self.chain)):
+                x, logpx = self.chain[i](x, logpx)
+            return x, logpx
 
-    def inverse(self, y, logpy=None, _logdetgrad_=None):
+    def inverse(self, y, logpy=None):
         if logpy is None:
             for i in range(len(self.chain) - 1, -1, -1):
                 y = self.chain[i].inverse(y)
             return y
         else:
-            if _logdetgrad_ is None:
-                for i in range(len(self.chain) - 1, -1, -1):
-                    y, logpy = self.chain[i].inverse(y, logpy)
-                return y, logpy
-            else:
-                for i in range(len(self.chain) - 1, -1, -1):
-                    y, logpy, _logdetgrad_ = self.chain[i].inverse(y, logpy, _logdetgrad_)
-                return y, logpy, _logdetgrad_               
+            for i in range(len(self.chain) - 1, -1, -1):
+                y, logpy = self.chain[i].inverse(y, logpy)
+            return y, logpy
 
 
 class Inverse(nn.Module):
@@ -46,8 +36,8 @@ class Inverse(nn.Module):
         super(Inverse, self).__init__()
         self.flow = flow
 
-    def forward(self, x, logpx=None, _logdetgrad_=None):
-        return self.flow.inverse(x, logpx, _logdetgrad_)
+    def forward(self, x, logpx=None):
+        return self.flow.inverse(x, logpx)
 
-    def inverse(self, y, logpy=None, _logdetgrad_=None):
-        return self.flow.forward(y, logpy, _logdetgrad_)
+    def inverse(self, y, logpy=None):
+        return self.flow.forward(y, logpy)
